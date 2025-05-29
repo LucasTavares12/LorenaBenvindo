138f95ab3b7fb5fdf79a636fba9f94cf77373250
LucasTavares12 <120190436+LucasTavares12@users.noreply.github.com>
2025-05-29 10:44:09 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -120,3 +120,9 @@ setInterval(() => {
       behavior: 'smooth'
     });
   }
+ const menuToggle = document.getElementById('menuToggle');
+  const navMenu = document.getElementById('navMenu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
